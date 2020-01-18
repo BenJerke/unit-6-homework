73675031ed881943d0e22a58c860545d777bd662
normalmn <appliedboredom@gmail.com>
2020-01-18 17:28:05 -0600

    fixing button issue

--- assets/javascript/giphy.js
+++ assets/javascript/giphy.js
@@ -1,59 +1,63 @@
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-
-
             // Initial array of gifs
             var gifs = ["Spongebob", "Ren & Stimpy", "Fairly Odd Parents", "Aqua Teen Hunger Force"];
-
+            console.log(gifs)
+            
             // Render the results returned in the AJAX call 
-            function displaygifInfo() {
+            function displayGifInfo() {
 
               var gif = $(this).attr("data-name");
-              var queryURL = "api.giphy.com/v1/gifs/search/q=" + gif + "&limit=30&api_key=T2i0lAHO0e6G7UwGRu6QIQxx0hlBMykx";
-
+              var queryURL = "https://api.giphy.com/v1/gifs/q=" + gif + "&limit=30&api_key=dc6zaTOxFJmzC";
+              console.log(gif);
               // When we click a button, make an AJAX call to the Giphy API with the button text as a search parameter, and then render the gifs in the gif-display div. 
+            
               $.ajax({
                 url: queryURL,
                 method: "GET"
               }).then(function(response) {
                 //parse the response into variables
-                var gifDiv = $("<div class='gif'>");
-                var rating = response.rating
-                var ratingp = $("<p>").text("Rating: " + rating);
-                gifDiv.append(ratingp);
-                var gifURLStill = response.images.fixed_height_still.url
-                var gifURLAnimate = response.images.fixed_height.url
-                var src = gifURLStill;
+                console.log(response)
+                
+
+                //var gifDiv = $("<div class='gif'>");
+                //var rating = response.rating
+                //var ratingp = $("<p>").text("Rating: " + rating);
+                //gifDiv.append(ratingp);
+                //var gifURLStill = response.images.fixed_height_still.url
+                //var gifURLAnimate = response.images.fixed_height.url
+                //var src = gifURLStill;
 
                 //render the response and add animation controls
-                var gifPic = $("<img>").attr({
-                  "src": src,  
-                  "data-state": "still", 
-                  "data-still": gifURLStill,
-                  "data-animate": gifURLAnimate,
-                  });
+                //var gifPic = $("<img>").attr({
+                //  "src": src,  
+                //  "data-state": "still", 
+                //  "data-still": gifURLStill,
+                //  "data-animate": gifURLAnimate,
+                //  });
 
-                gifDiv.append(gifPic);
-                $("#gif-display-col1").prepend(gifDiv);
+                //gifDiv.append(gifPic);
+               // $("#gif-display-col1").prepend(gifDiv);
               });
 
             }
-
+            
 
             function renderButtons() {
 
 
               $("#buttons-view").empty();
 
-              // for each entry in the gif array, make a new button classed with gif-btn, and append it to the buttons-view div. 
+              // for each entry in the gif array, make a new button classed with gifbutton, and append it to the buttons-view div. 
               for (var i = 0; i < gifs.length; i++) {
 
                 var a = $("<button>");
-                a.addClass("gif-btn");
+
+                a.addClass("gifbutton");
                 a.attr("data-name", gifs[i]);
                 a.text(gifs[i]);
                 $("#buttons-view").append(a);
               }
             }
+
 
 
             $("#add-gif").on("click", function(event) {
@@ -62,15 +66,20 @@
               var gif = $("#gif-input").val().trim();
 
               gifs.push(gif);
+              console.log(gifs);
 
               renderButtons();
+
             });
 
-            $(document).on("click", ".gifbutton", displaygifInfo);
+            $(document).on("click", ".gifbutton", displayGifInfo);
 
-            renderButtons();
+            
 
-            //when a gif is clicked, play the gif if it's still, and pause the gif if it's playing. 
+
+
+          //when a gif is clicked, play the gif if it's still, and pause the gif if it's playing. 
+
             function gifAnimate () {
               var state = $(this).attr("data-state");
           
@@ -96,3 +105,4 @@
           };
 
         }
+        renderButtons();
